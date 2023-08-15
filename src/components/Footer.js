b429008e8bd80a21fe7a76092a3160057828dcf4
faithnicoletti <faithalicia22@gmail.com>
2023-08-15 16:23:37 -0400

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,47 @@
+import "./FooterStyles.css";
+import {FaHome, FaPhone, FaMailBulk, FaGithub, FaLinkedin} from "react-icons/fa";
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footer-container">
+            <div className="left">
+                <div className="location">
+                    < FaHome size={20} style={{color:"#FFFFFF", marginRight:"2rem"}}/>
+                    <div>
+                        <p>Boston, Massachusetts</p>
+                    </div>
+                </div>
+                <div className="email">
+                    <FaMailBulk size={20} style={{color:"#FFFFFF", marginRight:"2rem"}}/>
+                    <div>
+                    <p>faithalicia22@gmail.com</p>
+                    </div>
+                </div>
+                <div className="phone">
+                    <h4><FaPhone size={20} style={{color:"#FFFFFF", marginRight:"2rem"}}/>
+                    1-518-570-9727</h4>
+                </div>
+            </div>
+            <div className="right">
+                <h4>More About Me:</h4>
+                <p>Check out my LinkedIn and 
+                    Github for more information</p>
+                    <div className="social">
+                        <FaLinkedin size={30} 
+                        style={{color:"#FFFFFF", 
+                        marginRight: "2rem"
+                        }}/>
+                        <FaGithub size={30} 
+                        style={{color:"#FFFFFF", 
+                        marginRight: "2rem"
+                        }}/>
+                    </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
