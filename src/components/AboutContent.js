ba0aa3bdd262bdbfe8ef1ae638ceabe6b8c8edf2
faithnicoletti <faithalicia22@gmail.com>
2023-08-15 18:54:58 -0400

    about page and projects page

---
+++ src/components/AboutContent.js
@@ -0,0 +1,37 @@
+import "./AboutContentStyles.css";
+import { Link } from "react-router-dom";
+import codingImage from "../assets/coding.png";
+
+import React from 'react'
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+        <div className="left">
+            <h1>Who Am I?</h1>
+            <p>I am a junior full stack software developer.</p>
+            <Link to="/contact">
+                <button className="btn">Contact</button>
+            </Link>
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={codingImage} 
+                    className="img"
+                    alt="true"/>
+                </div>
+                <div className="img-stack bottom">
+                    <img src={codingImage} 
+                    className="img"
+                    alt="true"/>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default AboutContent
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,24 @@
+import codingImage from "../assets/coding.png";
+
+const ProjectCardData = [
+    {
+        imgscr: codingImage,
+        title: "Ecommerce Website",
+        text: "Place text here", 
+        view: "github/faithnicoletti.com",
+    },
+    {
+        imgscr: codingImage,
+        title: "ChatApp",
+        text: "Place text here", 
+        view: "github/faithnicoletti.com",
+    }, 
+    {
+        imgscr: codingImage,
+        title: "Personal Blog",
+        text: "Place text here", 
+        view: "github/faithnicoletti.com",
+    }
+]
+
+export default ProjectCardData;
