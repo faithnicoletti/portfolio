a739bbdcb545027e9dee1799067a07537dfe95e1
faithnicoletti <faithalicia22@gmail.com>
2023-08-15 14:47:59 -0400

    home page styling/images and components

---
+++ src/components/HeroImg.js
@@ -0,0 +1,25 @@
+import "./HeroImgStyles.css";
+import IntroImg from "../assets/coding.png";
+import { Link } from "react-router-dom";
+import React from 'react'
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="into-img" 
+        src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>HI, I AM FAITH NICOLETTI</p>
+        <h1> Software Engineer.</h1>
+        <div>
+          <Link to="/projects" className="btn">Projects</Link>
+          <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroImg
